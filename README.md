02a41840408bbe17759095771eb5602db7ee17a1
kavya sree <kavyasree42@gmail.com>
2024-11-26 09:40:58 +0530

    data ingesion

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # StellarClassifierOps
 Pipeline for stellar classification
+
+## ML Pipeline
+
+1. Data Ingestion
+2. Data Transformation
+3. Model Trainer
+4. Model Evaluation
--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from src.StellarClassifier import logger
+from src.StellarClassifier.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 
-logger.info("Welcome to our custom logging Stellar Classification")
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.initiate_data_ingestion()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
